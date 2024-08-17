f6aad49cc7a1284fab238fe0a37965ac27c16d9b
viniciussouza429 <vinicius.souza429@hotmail.com>
2024-08-17 00:39:33 -0300

    Tela inicial - 17/08

--- Forms/U_Principal.pas
+++ Forms/U_Principal.pas
@@ -4,10 +4,45 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons, Vcl.ComCtrls,
+  Vcl.Menus;
 
 type
-  TForm1 = class(TForm)
+  TFrm_Principal = class(TForm)
+    Panel1: TPanel;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    SpeedButton5: TSpeedButton;
+    SpeedButton6: TSpeedButton;
+    SpeedButton7: TSpeedButton;
+    SpeedButton8: TSpeedButton;
+    SpeedButton9: TSpeedButton;
+    SpeedButton10: TSpeedButton;
+    StatusBar1: TStatusBar;
+    Timer1: TTimer;
+    MainMenu1: TMainMenu;
+    Cadastro1: TMenuItem;
+    Usurio1: TMenuItem;
+    Empresa1: TMenuItem;
+    Clientes1: TMenuItem;
+    Fornecedores1: TMenuItem;
+    Produtos1: TMenuItem;
+    FormasdePgto1: TMenuItem;
+    Movimentos1: TMenuItem;
+    Compras1: TMenuItem;
+    Vendas1: TMenuItem;
+    Relatorios1: TMenuItem;
+    ListaUsurios1: TMenuItem;
+    ListaFornecedores1: TMenuItem;
+    ListaClientes1: TMenuItem;
+    ListaProdutos1: TMenuItem;
+    ListaCompras1: TMenuItem;
+    ListaVendas1: TMenuItem;
+    Sobreosistema1: TMenuItem;
+    procedure Timer1Timer(Sender: TObject);
+    procedure SpeedButton10Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -15,10 +50,24 @@ type
   end;
 
 var
-  Form1: TForm1;
+  Frm_Principal: TFrm_Principal;
 
 implementation
 
 {$R *.dfm}
+
+procedure TFrm_Principal.SpeedButton10Click(Sender: TObject);
+begin
+ Close;
+end;
+
+procedure TFrm_Principal.Timer1Timer(Sender: TObject);
+begin
+// INSERE DADOS NA STATUSBAR
+
+StatusBar1.Panels[0].Text:=DateToStr(now);
+StatusBar1.Panels[1].Text:=TimeToStr(now);
+StatusBar1.Panels[2].Text:='Seja Bem-Vindo!'
+end;
 
 end.
--- Projeto/Controle_estoque.dpr
+++ Projeto/Controle_estoque.dpr
@@ -2,13 +2,13 @@ program Controle_estoque;
 
 uses
   Vcl.Forms,
-  U_Principal in '..\Forms\U_Principal.pas' {Form1};
+  U_Principal in '..\Forms\U_Principal.pas' {Frm_Principal};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TFrm_Principal, Frm_Principal);
   Application.Run;
 end.
